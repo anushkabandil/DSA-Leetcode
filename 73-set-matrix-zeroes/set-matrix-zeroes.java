4f185bba3a92a5f83b07ea6225959bfb714d8d84
anushka-bandil <bandilanushka@gmail.com>
2026-03-09 22:36:43 +0530

    Time: 1 ms (61.21%) | Memory: 47.8 MB (13.67%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] row = new int[m];
+        int[] colm = new int[n];
+
+        for(int i=0; i<m; i++){
+             for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    colm[j] = 1;
+
+                }
+             }
+        }
+
+         for(int i=0; i<m; i++){
+             for(int j=0; j<n; j++){
+                if(row[i] == 1 || colm[j] == 1 ){
+                   matrix[i][j] = 0;
+                }
+             }
+        }
+    }
+}
