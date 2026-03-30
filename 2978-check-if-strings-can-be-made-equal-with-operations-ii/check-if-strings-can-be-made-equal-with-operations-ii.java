572abd7112111b6d3f142bf1cee89f7060f57202
anushka-bandil <bandilanushka@gmail.com>
2026-03-30 23:13:46 +0530

    Time: 5 ms (98.68%) | Memory: 47.9 MB (85.53%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -2,23 +2,27 @@ class Solution {
     public boolean checkStrings(String s1, String s2) {
         if( s1.length() != s2.length()) return false;
 
-        Map<Character,Integer> even1 = new HashMap<>();
-        Map<Character,Integer> even2 = new HashMap<>();
-        Map<Character,Integer> odd1 = new HashMap<>();
-        Map<Character,Integer> odd2 = new HashMap<>();
+        int[] even = new int[26];
+        int[] odd = new int[26];
 
         for(int i=0; i<s1.length(); i++){
             char c1 = s1.charAt(i);
             char c2 = s2.charAt(i);
 
             if( i % 2 == 0){
-                even1.put(c1, even1.getOrDefault(c1,0)+1);
-                even2.put(c2, even2.getOrDefault(c2,0)+1);
+                even[c1 - 'a']++;
+                even[c2 - 'a']--;
             }else{
-                odd1.put(c1, odd1.getOrDefault(c1,0)+1);
-                odd2.put(c2, odd2.getOrDefault(c2,0)+1);
+                odd[c1 - 'a']++;
+                odd[c2 - 'a']--;
             }
         }
-        return even1.equals(even2) && odd1.equals(odd2);
+       
+       for(int i=0; i<26; i++){
+            if(even[i] != 0 || odd[i] != 0){
+                return false;
+            } 
+       }
+       return true;
     }
 }
