e240b53e1c62b5caa66c0d69503acef248b574ee
anushka-bandil <bandilanushka@gmail.com>
2026-05-01 11:34:04 +0530

    Time: 1561 ms (5.96%) | Memory: 48.8 MB (60.85%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -3,6 +3,7 @@ class Solution {
         int count = 0;
        for(int i =0; i<nums.length; i++){
             int sum = 0;
+            // means if a single number nums[i] equals k; taking such scenarios we use j=i.
             for(int j = i; j<nums.length; j++ ){
                sum += nums[j];
                 if(sum == k){
