ca9747af3ebb4ed2b7364935f66aa5f400dd3c36
anushka-bandil <bandilanushka@gmail.com>
2026-05-04 17:30:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (45.36%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,29 +1,62 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
+//         int m = obstacleGrid.length;
+//         int n = obstacleGrid[0].length;
 
-        int [][] dp = new int[m][n];
-        for(int[] r : dp){
-             Arrays.fill(r,-1);
+//         int [][] dp = new int[m][n];
+//         for(int[] r : dp){
+//              Arrays.fill(r,-1);
+//         }
+
+//         return solve(m-1, n-1, obstacleGrid,dp);
+
+//     }
+//     public static int solve(int m, int n, int[][] obstacleGrid, int [][] dp){
+
+//         if(m >= 0 && n >= 0 && obstacleGrid[m][n] == 1) return 0;
+//         if(m == 0 && n == 0) return 1;
+//         if(m < 0 || n < 0) return 0;
+
+// // MEMOIZAION
+//          if (dp[m][n] != -1) return dp[m][n];
+
+//         int up = solve(m-1, n, obstacleGrid, dp);
+//         int left = solve(m, n-1, obstacleGrid, dp);
+
+//         return dp[m][n] = up + left;
+//     }
+
+
+
+//TABULATION--------------------------------------------
+
+  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+     int m = obstacleGrid.length;
+     int n = obstacleGrid[0].length;
+     int [][] dp = new int[m][n];
+
+     if(obstacleGrid[0][0] == 1) return 0; 
+     dp[0][0] =1;
+
+     for(int i =0; i < m; i++){
+        for(int j =0; j < n; j++){
+
+            //obstacle
+            if(obstacleGrid[i][j] == 1){
+                dp[i][j] = 0;
+                continue;
+            }
+
+            //skip start cell 
+            if( i ==0 && j ==0) continue;
+
+            int up = (i>0)? dp[i-1][j] : 0;
+            int left = (j>0)? dp[i][j-1] : 0;
+
+            dp[i][j] = up + left;
         }
-
-        return solve(m-1, n-1, obstacleGrid,dp);
-
-    }
-    public static int solve(int m, int n, int[][] obstacleGrid, int [][] dp){
-
-        if(m >= 0 && n >= 0 && obstacleGrid[m][n] == 1) return 0;
-        if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
-
-// MEMOIZAION
-         if (dp[m][n] != -1) return dp[m][n];
-
-        int up = solve(m-1, n, obstacleGrid, dp);
-        int left = solve(m, n-1, obstacleGrid, dp);
-
-        return dp[m][n] = up + left;
-    }
+     }
+     return dp[m-1][n-1];
+  }
 }
