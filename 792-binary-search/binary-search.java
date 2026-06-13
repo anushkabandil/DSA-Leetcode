ba737a52b80536ce6868eb57f792de4d39f65c80
anushka-bandil <bandilanushka@gmail.com>
2026-06-13 14:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (13.89%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,9 +1,16 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i = 0; i< nums.length; i++)
-        {
-            if(nums[i] == target) return i;
-        }
-        return -1;
+        int n = nums.length - 1;
+        return binary(0, n ,target, nums);
+    }
+
+    public int binary(int start, int end, int target, int[] nums){
+
+        if(start > end ) return -1;
+        int mid = start +((end - start) / 2);
+
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] < target) return binary(mid+1, end, target , nums);
+        else return binary( start, mid-1,target, nums);
     }
 }
