852a9165ae49c842255b6f500c36eef5414b4e8b
anushka-bandil <bandilanushka@gmail.com>
2026-06-14 14:03:06 +0530

    Time: 61 ms (61.53%) | Memory: 44.4 MB (35.32%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,28 +1,57 @@
 class Solution {
+    // public int beautySum(String s) {
+    //     if( s.length() == 0 ) return 0;
+
+    //     int sum = 0;
+
+    //     for( int i = 0; i < s.length(); i++ )
+    //     {
+    //         HashMap<Character, Integer> freq = new HashMap<>();
+
+    //         for( int j = i; j < s.length() ; j++ )
+    //         {
+    //             freq.put(s.charAt(j) , freq.getOrDefault(s.charAt(j), 0)+1);
+
+    //             int maxi = Integer.MIN_VALUE;
+    //             int mini = Integer.MAX_VALUE;
+
+    //             for(int val : freq.values())
+    //             {
+    //                 mini = Math.min(mini, val);
+    //                 maxi = Math.max(maxi, val);
+    //             }
+    //             sum += ( maxi - mini );
+    //         }
+    //     }
+    //      return sum;
+    // }
+
+
+
     public int beautySum(String s) {
-        if( s.length() == 0 ) return 0;
 
         int sum = 0;
+        
+        for(int i = 0; i< s.length() ; i++){
+            int [] freq = new int[26];
 
-        for( int i = 0; i < s.length(); i++ )
-        {
-            HashMap<Character, Integer> freq = new HashMap<>();
+            for(int j = i; j< s.length() ; j++){
 
-            for( int j = i; j < s.length() ; j++ )
-            {
-                freq.put(s.charAt(j) , freq.getOrDefault(s.charAt(j), 0)+1);
+                freq[s.charAt(j) - 'a' ]++;
 
-                int maxi = Integer.MIN_VALUE;
+                int maxi = 0;
                 int mini = Integer.MAX_VALUE;
 
-                for(int val : freq.values())
-                {
-                    mini = Math.min(mini, val);
-                    maxi = Math.max(maxi, val);
+                for(int val : freq){
+
+                    if(val > 0){
+                        maxi = Math.max(maxi, val);
+                        mini = Math.min(mini, val);
+                    }
                 }
-                sum += ( maxi - mini );
+                sum += (maxi - mini);
             }
         }
-         return sum;
+        return sum;
     }
 }
